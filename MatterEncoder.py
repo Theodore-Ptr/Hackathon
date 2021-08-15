9f640bdb40e4aba4f59b57cf715e7cb2512a529f
Kiryl Khursik <55236898+lookafterh1m@users.noreply.github.com>
2021-08-15 13:35:55 +0300

    now-its-.py-and-with-dots

--- MatterEncoder.py
+++ MatterEncoder.py
@@ -1,16 +1,16 @@
-class MatterEncoder(BaseEstimator, TransformerMixin)
+class MatterEncoder(BaseEstimator, TransformerMixin):
     '''
     This class encodes the matters with goldsilver separation and respect to platemark
     '''
-    def __init__(self, drop=True)
+    def __init__(self, drop=True):
         BaseEstimator.__init__(self)
         TransformerMixin.__init__(self)
         self.drop = drop
     
-    def fit(self, X, y=None)
+    def fit(self, X, y=None):
         return self
     
-    def transform(self, X, y=None)
+    def transform(self, X, y=None):
         X['matter'] += X['platemark']
         X = pd.get_dummies(X,columns=['matter'])
         if self.drop == True
